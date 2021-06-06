46f85adb0c3100a2c789748252b33de997974283
Mudit Srivastava <60660510+muditbit@users.noreply.github.com>
2021-06-06 22:02:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ st.markdown(
     f"""
     <style>
     .reportview-container {{
-        background-color: #B0B1A6
+        background-image: url("https://github.com/muditbit/Major-Project/blob/master/test.jpg")
     }}
    
     """,
