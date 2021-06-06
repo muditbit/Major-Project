72ee8283ee074ccc86d8b2f8ca87f7837409e4cc
Mudit Srivastava <60660510+muditbit@users.noreply.github.com>
2021-06-06 19:49:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import joblib
+import base64
+st.set_page_config(layout='wide')
+st.markdown("""
+<style>
+.big-font {
+    font-size:50px !important;
+    font-family:Lucida Handwriting;
+    color:#D5C4C4;
+
+}
+
+</style>
+
+""", unsafe_allow_html=True)
+
+st.markdown("""
+<style>
+.small-font {
+    font-size: 18px !important;
+    font-family:Courier;
+    color:#DFF159;
+}
+</style>
+
+""", unsafe_allow_html=True)
+
+st.markdown("""
+<style>
+.medium-font {
+    font-size:20px !important;
+    font-family:Lucida Handwriting;
+    color:#D5C4C4;
+
+}
+</style>
+
+""", unsafe_allow_html=True)
+st.markdown('<p class="big-font">IMDB Review Sentiment Analysis !!</p>', unsafe_allow_html=True)
+
+def fun():
+  st.markdown('<p class="small-font">The website is a part of project of Internship.<br> The website deals with Sentiment analysis based on user review.<br> It generally take review of movies and predict the sentiment of user on the movie.</p>', unsafe_allow_html=True)
+  st.markdown('<p class="small-font">I would like to thank my team mates, mentor for their constant support and knowledge.</p>', unsafe_allow_html=True)  
+  return
+
+
+st.markdown('<p class="medium-font">Enter Review !!</p>', unsafe_allow_html=True)
+review = st.text_input("here")
+model = joblib.load('/content/drive/MyDrive/INternship/Major Project/IMDB Dataset.csv/imdb-rating')
+op = model.predict([review])
+if st.button('Analyse'):
+  st.markdown(f'<p class="medium-font"> The Review is {op[0]} </p>',unsafe_allow_html=True)
+
+
+
+main_bg = "/content/drive/MyDrive/INternship/Major Project/IMDB Dataset.csv/test.jpg"
+main_bg_ext = "jpg"
+
+
+
+st.markdown(
+    f"""
+    <style>
+    .reportview-container {{
+        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
+    }}
+   
+    """,
+    unsafe_allow_html=True
+)
+
+if st.sidebar.button('About'):
+  fun()
+
